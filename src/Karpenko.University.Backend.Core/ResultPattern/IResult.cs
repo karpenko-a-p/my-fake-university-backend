c82599abef3654dfa4c277b3bec98d72348747f5
Карпенко Александр Павлович <karpenkoap@vtb.ru>
2024-09-24 21:43:30 +0300

    Добавил слой Karpenko.University.Backend.Core

---
+++ src/Karpenko.University.Backend.Core/ResultPattern/IResult.cs
@@ -0,0 +1,6 @@
+﻿namespace Karpenko.University.Backend.Core.ResultPattern;
+
+/// <summary>
+/// Какой-либо результат
+/// </summary>
+public interface IResult;
---
+++ src/Karpenko.University.Backend.Core/ResultPattern/IValueResult.cs
@@ -0,0 +1,12 @@
+﻿namespace Karpenko.University.Backend.Core.ResultPattern;
+
+/// <summary>
+/// Параметризированный результат
+/// </summary>
+/// <typeparam name="TValue">Тип значения результата</typeparam>
+public interface IValueResult<out TValue> : IResult {
+  /// <summary>
+  /// Значение результата
+  /// </summary>
+  TValue Value { get; }
+}
