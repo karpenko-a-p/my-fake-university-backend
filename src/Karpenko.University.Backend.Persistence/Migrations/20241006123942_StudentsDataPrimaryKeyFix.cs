0a790b9650ee8c77096530242d2ae4884d8d2cfc
Карпенко Александр Павлович <karpenkoap@vtb.ru>
2024-10-06 16:01:35 +0300

    Добавил миграцию для изменения типа идентификаторов для таблицы студентов

---
+++ src/Karpenko.University.Backend.Persistence/Migrations/20241006123942_StudentsDataPrimaryKeyFix.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Karpenko.University.Backend.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentsDataPrimaryKeyFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "student_id",
+                schema: "university_backend",
+                table: "students_passwords",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,0)");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "id",
+                schema: "university_backend",
+                table: "students_data",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(20,0)")
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "student_id",
+                schema: "university_backend",
+                table: "students_passwords",
+                type: "numeric(20,0)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "id",
+                schema: "university_backend",
+                table: "students_data",
+                type: "numeric(20,0)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint")
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
