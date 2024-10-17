c83edd8790d94a174615f99b944e10e5e2fbd46e
Карпенко Александр Павлович <karpenkoap@vtb.ru>
2024-10-17 20:30:28 +0300

    Добавил абстрактный сервис для работы с кэшем

---
+++ src/Karpenko.University.Backend.Infrastructure/Caching/AbstractCacheService.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json.Converters;
+
+namespace Karpenko.University.Backend.Infrastructure.Caching;
+
+/// <summary>
+/// Абстрактный сервис со вспомогательными инструментами для кэширования
+/// </summary>
+internal abstract class AbstractCacheService(IDistributedCache cache) {
+  /// <summary>
+  /// Параметры сериализации
+  /// </summary>
+  private static readonly JsonSerializerSettings SerializerSettings = new() {
+    Converters = [new StringEnumConverter()]
+  };
+
+  /// <summary>
+  /// Кэширование какого-либо значения
+  /// </summary>
+  protected Task SetAsync<TValue>(
+    string key, 
+    TValue? value,
+    TimeSpan? slidingExpiration = null,
+    TimeSpan? absoluteExpirationRelativeToNow = null,
+    DateTimeOffset? absoluteExpiration = null
+  ) where TValue : class {
+    if (value is null)
+      return Task.CompletedTask;
+
+    var cacheOptions = new DistributedCacheEntryOptions {
+      SlidingExpiration = slidingExpiration ?? TimeSpan.FromMinutes(5),
+      AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow ?? TimeSpan.FromMinutes(30),
+      AbsoluteExpiration = absoluteExpiration,
+    };
+
+    var serializedValue = JsonConvert.SerializeObject(value, SerializerSettings);
+
+    return cache.SetAsync(key, Encoding.UTF8.GetBytes(serializedValue), cacheOptions);
+  }
+
+  /// <summary>
+  /// Получение значения из кэша
+  /// </summary>
+  protected async Task<TValue?> GetAsync<TValue>(string key) where TValue : class {
+    var value = await cache.GetStringAsync(key);
+
+    if (value is null)
+      return null;
+    
+    return JsonConvert.DeserializeObject<TValue>(value, SerializerSettings);
+  }
+}
