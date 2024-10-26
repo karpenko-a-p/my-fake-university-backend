2440e198ba1d921a30848d4f425252894392b7a0
Карпенко Александр Павлович <karpenkoap@vtb.ru>
2024-10-26 19:16:59 +0300

    Создал эндпоинты для взаимодействия с комментариями курсов

---
+++ src/Karpenko.University.Backend.API/Controllers/Comment/CommentController.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Mime;
+using Karpenko.University.Backend.Application.Pagination;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Karpenko.University.Backend.API.Controllers.Comment;
+
+/// <summary>
+/// Контроллер для работы комментариями
+/// </summary>
+[Consumes(MediaTypeNames.Application.Json)]
+[Produces(MediaTypeNames.Application.Json)]
+[Tags("api/comment/v1")]
+[Route("api/comment/v1")]
+public sealed class CommentController : ExtendedControllerBase {
+  /// <summary>
+  /// Получение комментариев определенного курса
+  /// </summary>
+  [HttpGet("by-course/{courseId:long:min(0)}")]
+  public async Task<IActionResult> GetCommentsByCourseIdAsync(
+    [FromRoute(Name = "courseId")] long courseId,
+    [FromQuery] PaginationModel pagination,
+    CancellationToken cancellationToken
+  ) {
+    return Ok();
+  }
+
+  /// <summary>
+  /// Получение комментариев написанных определенным человеком
+  /// </summary>
+  [HttpGet("by-owner/{ownerId:long:min(0)}")]
+  public async Task<IActionResult> GetCommentsByOwnerIdAsync(
+    [FromRoute(Name = "ownerId")] long ownerId,
+    [FromQuery] PaginationModel pagination,
+    CancellationToken cancellationToken
+  ) {
+    return Ok();
+  }
+
+  /// <summary>
+  /// Создание нового комментария
+  /// </summary>
+  [HttpPost]
+  public async Task<IActionResult> CreateCommentAsync(
+    CancellationToken cancellationToken
+  ) {
+    return Ok();
+  }
+
+  /// <summary>
+  /// Удаление комментария по идентификатору
+  /// </summary>
+  [HttpDelete("{commentId:long:min(0)}")]
+  public async Task<IActionResult> DeleteCommentByIdAsync(
+    [FromRoute(Name = "commentId")] long commentId,
+    CancellationToken cancellationToken
+  ) {
+    return Ok();
+  }
+}
---
+++ src/Karpenko.University.Backend.API/Controllers/Comment/Contracts/CommentContract.cs
@@ -0,0 +1,16 @@
+﻿using Karpenko.University.Backend.Domain.CourseComment;
+
+namespace Karpenko.University.Backend.API.Controllers.Comment.Contracts;
+
+/// <summary>
+/// Контракт для модели комментария
+/// </summary>
+/// <param name="Id">Идентификатор</param>
+/// <param name="Content">Содержание комментария</param>
+/// <param name="Quality">Оценка автора комментария курсу</param>
+/// <param name="CreationDate">Дата написания комментарий</param>
+public sealed record CommentContract(
+  long Id,
+  string Content,
+  CourseQuality Quality,
+  DateTime CreationDate);
